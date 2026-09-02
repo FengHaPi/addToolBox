2710ac12d6d535e58adb072fbe1959f0ac2e2ad0
FengHaPi <110076995+FengHaPi@users.noreply.github.com>
2026-09-02 11:37:03 +0800

    refactor: use fixed point 1d resize projection

--- src/AddToolBox.App/WorkspaceInteraction.cs
+++ src/AddToolBox.App/WorkspaceInteraction.cs
@@ -42,10 +42,38 @@ internal readonly record struct ResizeConstraintResult(
     int Iterations,
     int IterationLimit);
 
+internal enum AxisProjectionResultKind
+{
+    PreferredFastPath,
+    FixedProjectedSuccess,
+    FixedGridInfeasible,
+    NumericRangeExceeded,
+    NumericInvariantViolation,
+    InvalidInput
+}
+
+internal readonly record struct AxisProjectionItem(
+    string ToolId,
+    double PreferredStart,
+    double Size);
+
+internal readonly record struct AxisProjectionPosition(
+    string ToolId,
+    double Start,
+    double Size,
+    long? FixedStartTick);
+
+internal readonly record struct AxisProjectionResult(
+    AxisProjectionResultKind Kind,
+    AxisProjectionPosition[] Positions);
+
 internal static class WorkspaceInteraction
 {
+    internal const long TicksPerDip = 1024;
     internal const double SoftBoundaryPadding = 18;
     internal const double BoundaryContactEpsilon = 0.75;
+
+    private const long MaximumExactDoubleTick = 1L << 53;
 
     internal static SoftWorkspaceBounds GetSoftBounds(
         double workspaceWidth,
@@ -263,6 +291,345 @@ internal static class WorkspaceInteraction
         }
 
         return true;
+    }
+
+    internal static AxisProjectionResult Project1D(
+        AxisProjectionItem[] items,
+        double boundaryStart,
+        double boundaryEnd)
+    {
+        if (items is null
+            || !double.IsFinite(boundaryStart)
+            || !double.IsFinite(boundaryEnd)
+            || boundaryEnd < boundaryStart)
+        {
+            return AxisProjectionFailure(AxisProjectionResultKind.InvalidInput);
+        }
+
+        var uniqueToolIds = new HashSet<string>(StringComparer.Ordinal);
+        var orderedItems = (AxisProjectionItem[])items.Clone();
+        foreach (var item in orderedItems)
+        {
+            if (string.IsNullOrWhiteSpace(item.ToolId)
+                || !uniqueToolIds.Add(item.ToolId)
+                || !double.IsFinite(item.PreferredStart)
+                || !double.IsFinite(item.Size)
+                || item.Size <= 0)
+            {
+                return AxisProjectionFailure(AxisProjectionResultKind.InvalidInput);
+            }
+
+            var preferredCenter = item.PreferredStart + (item.Size / 2);
+            var preferredEnd = item.PreferredStart + item.Size;
+            if (!double.IsFinite(preferredCenter) || !double.IsFinite(preferredEnd))
+            {
+                return AxisProjectionFailure(AxisProjectionResultKind.NumericRangeExceeded);
+            }
+        }
+
+        Array.Sort(
+            orderedItems,
+            static (first, second) =>
+            {
+                var firstCenter = first.PreferredStart + (first.Size / 2);
+                var secondCenter = second.PreferredStart + (second.Size / 2);
+                var centerComparison = firstCenter.CompareTo(secondCenter);
+                return centerComparison != 0
+                    ? centerComparison
+                    : StringComparer.Ordinal.Compare(first.ToolId, second.ToolId);
+            });
+
+        if (IsLegalPreferredAxisLayout(orderedItems, boundaryStart, boundaryEnd))
+        {
+            var preferredPositions = new AxisProjectionPosition[orderedItems.Length];
+            for (var index = 0; index < orderedItems.Length; index++)
+            {
+                var item = orderedItems[index];
+                preferredPositions[index] = new AxisProjectionPosition(
+                    item.ToolId,
+                    item.PreferredStart,
+                    item.Size,
+                    null);
+            }
+
+            return new AxisProjectionResult(
+                AxisProjectionResultKind.PreferredFastPath,
+                preferredPositions);
+        }
+
+        if (!TryCeilingTick(boundaryStart, out var boundaryStartTick)
+            || !TryFloorTick(boundaryEnd, out var boundaryEndTick))
+        {
+            return AxisProjectionFailure(AxisProjectionResultKind.NumericRangeExceeded);
+        }
+
+        try
+        {
+            return ProjectFixed1D(orderedItems, boundaryStartTick, boundaryEndTick,
+                boundaryStart, boundaryEnd);
+        }
+        catch (OverflowException)
+        {
+            return AxisProjectionFailure(AxisProjectionResultKind.NumericRangeExceeded);
+        }
+    }
+
+    private static AxisProjectionResult ProjectFixed1D(
+        AxisProjectionItem[] orderedItems,
+        long boundaryStartTick,
+        long boundaryEndTick,
+        double originalBoundaryStart,
+        double originalBoundaryEnd)
+    {
+        var itemCount = orderedItems.Length;
+        var sizeTicks = new long[itemCount];
+        var preferredTicks = new long[itemCount];
+        for (var index = 0; index < itemCount; index++)
+        {
+            if (!TryCeilingTick(orderedItems[index].Size, out sizeTicks[index])
+                || sizeTicks[index] <= 0
+                || !TryNearestEvenTick(
+                    orderedItems[index].PreferredStart,
+                    out preferredTicks[index]))
+            {
+                return AxisProjectionFailure(AxisProjectionResultKind.NumericRangeExceeded);
+            }
+        }
+
+        var offsets = new long[itemCount];
+        var totalSizeTick = 0L;
+        for (var index = 0; index < itemCount; index++)
+        {
+            offsets[index] = totalSizeTick;
+            totalSizeTick = checked(totalSizeTick + sizeTicks[index]);
+        }
+
+        var axisSpanTick = checked(boundaryEndTick - boundaryStartTick);
+        if (totalSizeTick > axisSpanTick)
+        {
+            return AxisProjectionFailure(AxisProjectionResultKind.FixedGridInfeasible);
+        }
+
+        var lowerTick = boundaryStartTick;
+        var upperTick = checked(boundaryEndTick - totalSizeTick);
+        var blocks = new AxisProjectionBlock[itemCount];
+        var blockCount = 0;
+        for (var index = 0; index < itemCount; index++)
+        {
+            var targetTick = checked(preferredTicks[index] - offsets[index]);
+            blocks[blockCount++] = new AxisProjectionBlock(
+                index,
+                index,
+                targetTick,
+                1);
+
+            while (blockCount >= 2
+                   && CompareBlockMeans(
+                       blocks[blockCount - 2],
+                       blocks[blockCount - 1]) > 0)
+            {
+                var previous = blocks[blockCount - 2];
+                var current = blocks[blockCount - 1];
+                blocks[blockCount - 2] = new AxisProjectionBlock(
+                    previous.FirstIndex,
+                    current.LastIndex,
+                    checked(previous.SumTicks + current.SumTicks),
+                    checked(previous.Weight + current.Weight));
+                blockCount--;
+            }
+        }
+
+        var projectedTicks = new long[itemCount];
+        long? previousProjectedTick = null;
+        for (var blockIndex = 0; blockIndex < blockCount; blockIndex++)
+        {
+            var block = blocks[blockIndex];
+            long projectedTick;
+            if (CompareBlockMeanToTick(block, lowerTick) < 0)
+            {
+                projectedTick = lowerTick;
+            }
+            else if (CompareBlockMeanToTick(block, upperTick) > 0)
+            {
+                projectedTick = upperTick;
+            }
+            else
+            {
+                projectedTick = RationalNearestEven(block.SumTicks, block.Weight);
+            }
+
+            if (previousProjectedTick is { } previous
+                && projectedTick < previous)
+            {
+                return AxisProjectionFailure(
+                    AxisProjectionResultKind.NumericInvariantViolation);
+            }
+
+            for (var itemIndex = block.FirstIndex;
+                 itemIndex <= block.LastIndex;
+                 itemIndex++)
+            {
+                projectedTicks[itemIndex] = projectedTick;
+            }
+
+            previousProjectedTick = projectedTick;
+        }
+
+        var startTicks = new long[itemCount];
+        var endTicks = new long[itemCount];
+        for (var index = 0; index < itemCount; index++)
+        {
+            startTicks[index] = checked(projectedTicks[index] + offsets[index]);
+            endTicks[index] = checked(startTicks[index] + sizeTicks[index]);
+            if (!IsExactlyRepresentableTick(startTicks[index])
+                || !IsExactlyRepresentableTick(endTicks[index])
+                || startTicks[index] < boundaryStartTick
+                || endTicks[index] > boundaryEndTick
+                || (index > 0 && startTicks[index] < endTicks[index - 1]))
+            {
+                return AxisProjectionFailure(
+                    AxisProjectionResultKind.NumericInvariantViolation);
+            }
+        }
+
+        var resolvedPositions = new AxisProjectionPosition[itemCount];
+        for (var index = 0; index < itemCount; index++)
+        {
+            var start = startTicks[index] / (double)TicksPerDip;
+            var actualEnd = start + orderedItems[index].Size;
+            if (!double.IsFinite(start)
+                || !double.IsFinite(actualEnd)
+                || start < originalBoundaryStart
+                || actualEnd > originalBoundaryEnd
+                || (index > 0
+                    && start
+                    < resolvedPositions[index - 1].Start
+                    + resolvedPositions[index - 1].Size))
+            {
+                return AxisProjectionFailure(
+                    AxisProjectionResultKind.NumericInvariantViolation);
+            }
+
+            resolvedPositions[index] = new AxisProjectionPosition(
+                orderedItems[index].ToolId,
+                start,
+                orderedItems[index].Size,
+                startTicks[index]);
+        }
+
+        return new AxisProjectionResult(
+            AxisProjectionResultKind.FixedProjectedSuccess,
+            resolvedPositions);
+    }
+
+    private static bool IsLegalPreferredAxisLayout(
+        AxisProjectionItem[] orderedItems,
+        double boundaryStart,
+        double boundaryEnd)
+    {
+        for (var index = 0; index < orderedItems.Length; index++)
+        {
+            var item = orderedItems[index];
+            var end = item.PreferredStart + item.Size;
+            if (item.PreferredStart < boundaryStart
+                || end > boundaryEnd
+                || (index > 0
+                    && item.PreferredStart
+                    < orderedItems[index - 1].PreferredStart
+                    + orderedItems[index - 1].Size))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static AxisProjectionResult AxisProjectionFailure(
+        AxisProjectionResultKind kind)
+    {
+        return new AxisProjectionResult(kind, []);
+    }
+
+    private static int CompareBlockMeans(
+        AxisProjectionBlock first,
+        AxisProjectionBlock second)
+    {
+        var left = (Int128)first.SumTicks * second.Weight;
+        var right = (Int128)second.SumTicks * first.Weight;
+        return left.CompareTo(right);
+    }
+
+    private static int CompareBlockMeanToTick(
+        AxisProjectionBlock block,
+        long tick)
+    {
+        var left = (Int128)block.SumTicks;
+        var right = (Int128)tick * block.Weight;
+        return left.CompareTo(right);
+    }
+
+    private static long RationalNearestEven(long sumTicks, int weight)
+    {
+        if (weight <= 0)
+        {
+            throw new InvalidOperationException("PAV block weight must be positive.");
+        }
+
+        var quotient = sumTicks / weight;
+        var remainder = sumTicks % weight;
+        var doubledRemainderMagnitude = Math.Abs((long)remainder) * 2;
+        if (doubledRemainderMagnitude < weight)
+        {
+            return quotient;
+        }
+
+        var direction = Math.Sign(sumTicks);
+        if (doubledRemainderMagnitude > weight)
+        {
+            return checked(quotient + direction);
+        }
+
+        return (quotient & 1) == 0
+            ? quotient
+            : checked(quotient + direction);
+    }
+
+    private static bool TryCeilingTick(double value, out long tick)
+    {
+        return TryScaledTick(Math.Ceiling(value * TicksPerDip), out tick);
+    }
+
+    private static bool TryFloorTick(double value, out long tick)
+    {
+        return TryScaledTick(Math.Floor(value * TicksPerDip), out tick);
+    }
+
+    private static bool TryNearestEvenTick(double value, out long tick)
+    {
+        return TryScaledTick(
+            Math.Round(value * TicksPerDip, MidpointRounding.ToEven),
+            out tick);
+    }
+
+    private static bool TryScaledTick(double scaledValue, out long tick)
+    {
+        if (!double.IsFinite(scaledValue)
+            || scaledValue < -MaximumExactDoubleTick
+            || scaledValue > MaximumExactDoubleTick)
+        {
+            tick = 0;
+            return false;
+        }
+
+        tick = (long)scaledValue;
+        return true;
+    }
+
+    private static bool IsExactlyRepresentableTick(long tick)
+    {
+        return tick >= -MaximumExactDoubleTick
+            && tick <= MaximumExactDoubleTick;
     }
 
     private static void ClampToolsToBounds(
@@ -497,4 +864,10 @@ internal static class WorkspaceInteraction
 
         return requiredSpan;
     }
+
+    private readonly record struct AxisProjectionBlock(
+        int FirstIndex,
+        int LastIndex,
+        long SumTicks,
+        int Weight);
 }
